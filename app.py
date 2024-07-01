08a0c6f4ae0f2a004639813a180c2e93a4052e70
Sunil Kappal <skappal7@users.noreply.github.com>
2024-07-01 21:25:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,10 @@ def download_csv(data, filename):
 st.title('Google Play Store Review Scraper')
 
 st.write("To find the app ID, go to the Google Play Store, search for the app, and copy the part of the URL after `id=` (e.g., for `https://play.google.com/store/apps/details?id=com.example.app`, the app ID is `com.example.app`).")
+st.write("[Click here for a guide on how to find the Google Play Store app ID](https://www.sociablekit.com/how-to-find-google-play-app-id/)")
+
 app_id = st.text_input('Enter the Google Play App ID:')
-num_reviews = st.slider('Select number of reviews to scrape', min_value=100, max_value=5000, step=100, value=100)
+num_reviews = st.slider('Select number of reviews to scrape', min_value=1, max_value=1000, step=100, value=1)
 sort_order = st.selectbox('Select the sort order of the reviews', ['Newest', 'Rating'])
 sort_order_map = {'Newest': Sort.NEWEST, 'Rating': Sort.RATING}
 sort_order_selected = sort_order_map[sort_order]
